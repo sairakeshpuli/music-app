e617c6544bf2a664bcb2e03f4c8343495c80c32d
rakesh <rakesh.puli97@gmail.com>
2022-08-02 19:15:19 +0530

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [tabdata, setTabdata] = useState([]);
+  useEffect(() => {
+    fetch('https://62da7ec69eedb699636fc58d.mockapi.io/todolist')
+      .then(response => response.json())
+      .then(json => setTabdata(json))
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 style={{ "fontFamily": "Roboto", "color": "yellow", "backgroundColor": "" }}> Music App</h1>
+      <hr style={{ "width": "260px" }} />
+      {tabdata.map((info) =>
+        <div className='items'>
+          <h1 style={{ "fontFamily": "Roboto", "color": "white" }}>{info.movie}</h1>
+
+          <img src={info.img} alt="img" width={"300px"} height={"300px"} />
+          <h3 style={{ "fontFamily": "Roboto", "color": "white" }}> {info.title}</h3>
+          <audio controls >
+            <source src={info.song} type="audio/mp3" />
+          </audio>
+          <hr style={{ "width": "300px" }} />
+
+        </div>)}
+
     </div>
+
   );
+
 }
 
 export default App;
